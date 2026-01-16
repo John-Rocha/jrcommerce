257be47e1ef9edf95f87d929fdef999950be9418
Johnathan Rocha <johnathanrocha@gmail.com>
2026-01-16 11:16:43 -0300

    feat: entidade User, banco H2

---
+++ src/main/java/com/jrcommerce/jrcommerce/JrcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jrcommerce.jrcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrcommerce/jrcommerce/JrcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrcommerce.jrcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
