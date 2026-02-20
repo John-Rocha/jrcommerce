84e6f0082ab63298eebfa7667ee6300959157f56
Johnathan Rocha <johnathanrocha@gmail.com>
2026-02-20 13:33:31 -0300

    feat: update findAll methods in ProductController and ProductService to support pagination

--- src/main/java/com/jrcommerce/jrcommerce/controllers/ProductController.java
+++ src/main/java/com/jrcommerce/jrcommerce/controllers/ProductController.java
@@ -1,8 +1,8 @@
 package com.jrcommerce.jrcommerce.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +23,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductDTO> findAll() {
-        return productService.findAll();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
--- src/main/java/com/jrcommerce/jrcommerce/services/ProductService.java
+++ src/main/java/com/jrcommerce/jrcommerce/services/ProductService.java
@@ -1,8 +1,8 @@
 package com.jrcommerce.jrcommerce.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,8 +23,8 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<ProductDTO> findAll() {
-        List<Product> products = productRepository.findAll();
-        return products.stream().map(product -> new ProductDTO(product)).toList();
+    public org.springframework.data.domain.Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = productRepository.findAll(pageable);
+        return products.map(product -> new ProductDTO(product));
     }
 }
